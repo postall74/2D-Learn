007a6551dc81e3d8a68ee5533ffb02a1edf1b863
postall_mcs <postall_mcs@mail.ru>
2025-03-18 17:24:52 +0500

    Initial commit

---
+++ Assets/Scripts/GrowthCapsule.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider), typeof(MeshRenderer))]
+public class GrowthCapsule : MonoBehaviour
+{
+    [SerializeField] private Transform _transform;
+    [SerializeField] private float _growSpeed = 0.5f;
+
+    private void Start()
+    {
+        if (_transform == null)
+            _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        ScaleCapsule(_transform, _growSpeed);
+    }
+
+    private void ScaleCapsule(Transform transform, float growSpeed)
+    {
+        transform.transform.localScale += growSpeed * Time.deltaTime * Vector3.one;
+    }
+}
